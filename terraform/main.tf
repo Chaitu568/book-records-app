e8fc75dd027af53195b12e0bf48010eede3fd337
Chaitu568 <cpoluri@asu.edu>
2025-01-14 14:57:59 -0500

    duplicate variables remmoved

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,21 +23,3 @@ resource "heroku_app" "fastapi_app" {
     "heroku/python"
   ]
 }
-
-# variables.tf
-variable "app_name" {
-  description = "Name of the Heroku application"
-  type        = string
-}
-
-variable "region" {
-  description = "Heroku region"
-  type        = string
-  default     = "us"
-}
-
-# outputs.tf
-output "app_url" {
-  value       = "https://${heroku_app.fastapi_app.name}.herokuapp.com"
-  description = "Application URL"
-}
